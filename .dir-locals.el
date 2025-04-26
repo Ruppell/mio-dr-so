8702a9c2fe92244fec10230e834952a70945b3c0
Vernon Grant <vernon@ruppell.io>
2025-04-26 17:49:07 +0800

    feat: Initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+(
+ (nil . ((vg-project-tasks . (
+                              ("Serve" . (lambda()
+                                                     (vg-multi-term-shell-command "npm run serve")))
+                              ("Tailwind: Watch" . (lambda()
+                                                     (vg-multi-term-shell-command "npm run tailwind:watch")))
+                              ))))
+ )
